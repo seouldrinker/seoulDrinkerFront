3f212b62454eb6041574cc71f24f5c7500f95d05
jicjjang <jicjjang12@gmail.com>
2017-10-06 23:49:00 +0900

    somthing change

---
+++ app/styles/index.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+})
--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry } from 'react-native'
+import React, { Component } from 'react'
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
 
-AppRegistry.registerComponent('seoulDrinkerFront', () => App);
+import commonStyles from './app/styles/common'
+import indexStyles from './app/styles'
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+})
+
+class App extends Component<{}> {
+  render() {
+    return (
+      <View style={StyleSheet.flatten([commonStyles.container, indexStyles.container])}>
+        <Text style={indexStyles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={indexStyles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={indexStyles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    )
+  }
+}
+
+AppRegistry.registerComponent('seoulDrinkerFront', () => App)
