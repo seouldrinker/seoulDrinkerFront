bb4c2a7e8ad3ad3dd7ea5e5dd16399e347fbe832
jicjjang <jicjjang12@gmail.com>
2017-10-14 22:14:47 +0900

    set camera modules

--- android/app/src/main/java/com/seouldrinkerfront/MainApplication.java
+++ android/app/src/main/java/com/seouldrinkerfront/MainApplication.java
@@ -3,6 +3,7 @@ package com.seouldrinkerfront;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.magus.fblogin.FacebookLoginPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new ImagePickerPackage(),
           new FacebookLoginPackage(),
           new SplashScreenReactPackage(),
           new RNGoogleSigninPackage()
