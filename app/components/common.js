b6434335bdc897faadb0e370df00b198b7fe32f9
jicjjang <jicjjang12@gmail.com>
2017-11-09 08:16:53 +0900

    append makeTimer excepted seconds

--- app/components/common.js
+++ app/components/common.js
@@ -18,6 +18,16 @@ export function makeTimer(propsDate) {
   return `${feedDate.getMonth()+1}월 ${feedDate.getDate()}일 ${feedHour}:${feedMinutes}`
 }
 
+export function makeTimerNoSeconds(propsDate) {
+  const feedDate = new Date(propsDate)
+  const feedHour = (feedDate.getHours() > 12) ?
+    `오후 ${feedDate.getHours()-12}` :
+    ((feedDate.getHours() === 0) ? `오전 12` : `오전 ${feedDate.getHours()}`)
+  const feedMinutes = ((feedDate.getMinutes() < 10) ? '0': '') + feedDate.getMinutes()
+
+  return `${feedDate.getMonth()+1}월 ${feedDate.getDate()}일`
+}
+
 export class BaseList extends Component {
   componentDidMount () {
   }
